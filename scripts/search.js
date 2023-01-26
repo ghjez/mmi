634b1567aaec8fd4d6b2891977bdc39f36b95d5d
ghjez <g.danylyshyn@gmail.com>
2023-01-26 08:58:45 +0100

    cleanup

--- scripts/search.js
+++ scripts/search.js
@@ -6,5 +6,5 @@ console.log(search_btn);
 
 search_btn.addEventListener("click", () => {
     if(searchbox.value == ("Informatik"||"informatik"||"info"||"Info")) window.location.href="modul_search.html";
-    else alert("Poo poo");
+    else alert("Suchen Sie nach 'Informatik'");
 })
